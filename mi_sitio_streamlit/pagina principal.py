a4197ff323481a4ac37207343d86b1c2b040396a
nicolasalejandro1910-droid <nicolas.alejandro1910@gmail.com>
2025-09-23 19:23:13 -0300

    pagina principal.py

--- mi_sitio_streamlit/pagina principal.py
+++ mi_sitio_streamlit/pagina principal.py
@@ -18,7 +18,7 @@ st.markdown("---") # Separador
 
 # Imagenes de la carpeta 'assets'
 st.header("¿Por qué elegir un perfume?")
-st.image(os.path.join("assets", "imagen_perfume1.jpg"), caption="Una fragancia puede definir tu personalidad.", use_column_width=True)
+caption="Una fragancia puede definir tu personalidad.", use_column_width=True)
 
 # Textos
 st.write("Un perfume es mucho más que un aroma; es una declaración de estilo, un recuerdo y una extensión de tu personalidad. Cada fragancia cuenta una historia diferente en la piel de quien la lleva.")
@@ -26,3 +26,4 @@ st.write("Un perfume es mucho más que un aroma; es una declaración de estilo, 
 # Interacción con el usuario
 if st.button("¡Haz clic para una curiosidad sobre perfumes!"):
     st.info("¿Sabías que la palabra 'perfume' proviene del latín 'per fumum', que significa 'a través del humo'?")
+
