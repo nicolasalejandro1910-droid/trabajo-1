52c9bceeb778078d273ec0c73efebe9aa22c6a08
nicolasalejandro1910-droid <nicolas.alejandro1910@gmail.com>
2025-09-23 20:05:48 -0300

    pagina principal.py

--- mi_sitio_streamlit/pagina principal.py
+++ mi_sitio_streamlit/pagina principal.py
@@ -15,6 +15,14 @@ st.title("🌸 El Universo de los Perfumes Femeninos 🌸")
 st.write("¡Bienvenida a un viaje sensorial por el fascinante mundo de las fragancias! Aquí descubrirás las historias, familias olfativas y secretos detrás de los perfumes más icónicos.")
 
 st.markdown("---") # Separador
+import streamlit as st
+import os
+
+# Si la imagen es local
+
+# Si la imagen es desde una URL
+url_de_la_imagen = "https://github.com/nicolasalejandro1910-droid/trabajo-1/blob/main/mi_sitio_streamlit/assets/foto1.jpg?raw=true"
+st.image(url_de_la_imagen, width=600, caption="Imagen de referencia")
 
 # Imagenes de la carpeta 'assets'
 st.header("¿Por qué elegir un perfume?")
@@ -25,6 +33,7 @@ st.write("Un perfume es mucho más que un aroma; es una declaración de estilo, 
 # Interacción con el usuario
 if st.button("¡Haz clic para una curiosidad sobre perfumes!"):
     st.info("¿Sabías que la palabra 'perfume' proviene del latín 'per fumum', que significa 'a través del humo'?")
+
 
 
 
