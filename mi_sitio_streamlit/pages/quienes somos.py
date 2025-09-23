bd1589eeb82fa02afa4f6bec5274b6c1d09393b9
nicolasalejandro1910-droid <nicolas.alejandro1910@gmail.com>
2025-09-23 19:22:01 -0300

    quienes somos.py

--- mi_sitio_streamlit/pages/quienes somos.py
+++ mi_sitio_streamlit/pages/quienes somos.py
@@ -6,7 +6,6 @@ st.title("Sobre Nosotras")
 st.markdown("---")
 
 st.header("Nuestra pasión por los aromas")
-st.image("https://images.unsplash.com/photo-1596707328224-d2e8b26e64c3", caption="Equipo de Ficticia Fragrances S.A.")
 
 st.write("""
 **Fragrances IeJ S.A.** Es una empresa ficticia dedicada a la divulgación de la cultura del perfume. Fundada en 2025 por una perfumistas y amantes de las fragancias con el único propósito de compartir conocimientos y secretos sobre este arte.
@@ -18,4 +17,5 @@ Nuestro equipo está compuesto por:
 
 """)
 st.warning("Toda la información y los perfumes son exclusivos de la dueña.")
+
 
