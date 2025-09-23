05d8394f4247a8db14693a0396fe7c2ba188a1ff
nicolasalejandro1910-droid <nicolas.alejandro1910@gmail.com>
2025-09-23 20:09:29 -0300

    pagina principal.py

--- mi_sitio_streamlit/pagina principal.py
+++ mi_sitio_streamlit/pagina principal.py
@@ -18,11 +18,16 @@ st.markdown("---") # Separador
 import streamlit as st
 import os
 
-# Si la imagen es local
-
 # Si la imagen es desde una URL
 url_de_la_imagen = "https://github.com/nicolasalejandro1910-droid/trabajo-1/blob/main/mi_sitio_streamlit/assets/logo.png?raw=true"
-st.image(url_de_la_imagen, width=400)
+
+# Crea tres columnas con un ratio de 1:2:1
+# Esto hace que la columna central sea el doble de ancha que las laterales
+col1, col2, col3 = st.columns([1, 2, 1])
+
+with col2:
+    # Muestra la imagen dentro de la columna del centro
+    st.image(url_de_la_imagen, width=400)
 
 # Imagenes de la carpeta 'assets'
 st.header("¿Por qué elegir un perfume?")
@@ -33,6 +38,7 @@ st.write("Un perfume es mucho más que un aroma; es una declaración de estilo, 
 # Interacción con el usuario
 if st.button("¡Haz clic para una curiosidad sobre perfumes!"):
     st.info("¿Sabías que la palabra 'perfume' proviene del latín 'per fumum', que significa 'a través del humo'?")
+
 
 
 
