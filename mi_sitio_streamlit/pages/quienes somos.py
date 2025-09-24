b4c439dba4e6b7dbaf869ead55e9d57d899a6661
nicolasalejandro1910-droid <nicolas.alejandro1910@gmail.com>
2025-09-23 22:02:20 -0300

    quienes somos.py

--- mi_sitio_streamlit/pages/quienes somos.py
+++ mi_sitio_streamlit/pages/quienes somos.py
@@ -8,7 +8,7 @@ st.markdown("---")
 st.header("Nuestra pasión por los aromas")
 
 st.write("""
-**Fragrances IeJ S.A.** Es una empresa ficticia dedicada a la divulgación de la cultura del perfume. Fundada en 2025 por una perfumistas y amantes de las fragancias con el único propósito de compartir conocimientos y secretos sobre este arte.
+**Jasmin et Iris S.A.** Es una empresa ficticia dedicada a la divulgación de la cultura del perfume. Fundada en 2025 por una perfumistas y amantes de las fragancias con el único propósito de compartir conocimientos y secretos sobre este arte.
 
 Nuestro equipo está compuesto por:
 * **Javiera:** Perfumista experta.
@@ -17,6 +17,7 @@ Nuestro equipo está compuesto por:
 
 """)
 st.warning("Toda la información y los perfumes son exclusivos de la dueña.")
+
 
 
 
